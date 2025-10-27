74f5ebb0990845a76369390ad9311ed6bece5b30
CUSTcoding <custodiotitossetitosse@gmail.com>
2025-10-27 18:46:03 -0500

    feat( src/app/servicos/): add pages service

---
+++ src/app/servicos/page.tsx
@@ -0,0 +1,17 @@
+import Logo from "@/components/header/logo";
+import Navbar from "@/components/header/menu";
+import About from "@/components/about/about";
+import Footer from "@/components/footer/footer";
+
+export default function Servicos(){
+    return(
+        <main>
+            <Logo/>
+            <Navbar/>
+            
+            <About/>
+            <Footer/>
+
+        </main>
+    );
+}
