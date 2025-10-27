4bdf296f958474b55bee32e1e65900d0750eb28f
CUSTcoding <custodiotitossetitosse@gmail.com>
2025-10-27 18:47:25 -0500

    feat(src/app/contacto/): add pages contact

---
+++ src/app/contacto/page.tsx
@@ -0,0 +1,15 @@
+import Logo from "@/components/header/logo";
+import Navbar from "@/components/header/menu";
+import About from "@/components/about/about";
+import Footer from "@/components/footer/footer";
+
+export default function Contacto(){
+    return(
+        <main>
+            <Logo/>
+            <Navbar/>
+            <About/>
+            <Footer/>
+        </main>
+    );
+}
