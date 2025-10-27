5132c9e7232316fc72aacaa2742218d1a1c24035
CUSTcoding <custodiotitossetitosse@gmail.com>
2025-10-27 18:23:44 -0500

    feat(heropage) add a main message

--- src/components/heropage/heropage.tsx
+++ src/components/heropage/heropage.tsx
@@ -1,5 +1,17 @@
+import { section } from "framer-motion/client";
+
 export default function Hero() {
   return (
-    <main className="flex items-center justify-center w-screen min-h-screen">home</main>
+    <section className="home flex items-center min-h-screen flex-col md:flex-row justify-center gap-2 w-[calc(95%)] md:w-[calc(70%)]">
+        <div className="w-1/2 bg-white">
+        </div>
+       <div className=" md:w-1/2 flex flex-col gap-2 w-full items-center justify-center md:items-start">
+            <h1 className="text-3xl font-semibold text-center md:text-start">José Arone Mulate Chambal</h1>
+            <p className="text-center md:text-start text-lg">Líder jovem, apaixonado por inclusão, justiça social e transformação.</p>
+            <button  className="flex items-start md:items-center">
+                <a href="#about" className="border px-3 py-1.5 rounded-full hover:bg-white hover:text-black hover:font-semibold duration-500 transition ">Saiba mais</a>
+            </button>
+       </div>
+    </section>
   );
 }
